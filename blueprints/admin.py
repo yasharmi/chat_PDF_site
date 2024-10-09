28f276fe0a680e347d071829efcc17fff52d03f8
yashar <yashar.mohseni.1384@gmail.com>
2024-10-09 16:41:13 +0330

    make user editor dashboard

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template, request, session, redirect, abort
+from flask import Blueprint, render_template, request, session, redirect, abort, url_for
 import config
+from models.user import User
+from extentions import db
 
 app = Blueprint("admin", __name__)
+
+
+
+@app.before_request
+def before_request():
+    if session.get('admin_login', None) == None and request.endpoint != "admin.login":
+        abort(403)
 
 
 
@@ -23,7 +32,26 @@ def login():  # put application's code here
 
 @app.route('/admin/dashboard' , methods = ["GET"])
 def dashboard():
-    if session.get('admin_login', None) == None:
-        abort(403)
+    return render_template("admin/dashboard.html")
 
-    return "dashboard"
+@app.route('/admin/dashboard/user_editor' , methods=["GET", "POST"])
+def user_editor():
+    if request.method == "GET":
+
+        user = User.query.all()
+
+        return render_template("admin/user_editor.html", user=user)
+    else:
+        username = request.form.get('username',None)
+        email = request.form.get('email',None)
+        phone = request.form.get('phone',None)
+
+        p = User(username=username, email=email, phone=phone)
+        db.session.add(p)
+        db.session.commit()
+
+        return "done"
+
+
+
+
