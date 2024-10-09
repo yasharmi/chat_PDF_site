caeffceed55d837399047c1167edf48cd9160b8b
yashar <yashar.mohseni.1384@gmail.com>
2024-10-09 14:37:04 +0330

    admin login & admin dashboard

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from blueprints.general import app as general
 from blueprints.user import app as user
+from blueprints.admin import app as admin
 import config
 import extentions
 
@@ -9,9 +10,11 @@ import extentions
 app = Flask(__name__)
 app.register_blueprint(general)
 app.register_blueprint(user)
+app.register_blueprint(admin)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+app.secret_key = config.SECRET_KEY
 extentions.db.init_app(app)
 
 
--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "1234"
+
+
+SECRET_KEY = "mvngjdmkahvdh$12345hjwmihudfgfiknefjn23"
